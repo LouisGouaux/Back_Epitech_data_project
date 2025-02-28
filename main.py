d12afe4d11d542eb9243d16ff85dab3c05b20c7d
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2025-02-28 23:47:30 +0100

    Refactor: Made get_aggregated_data reusable

--- main.py
+++ main.py
@@ -16,28 +16,39 @@ def get_resources(date):
     return (df_filtered[["date"] + resource_column].to_dict(orient="records"))
 
 
-def get_admissions_data():
+def get_aggregated_data(aggregate_column, value_order=None, value_columns=None):
     df = import_dataset()
-    df_weekly = df.groupby("day_of_week")[["admissions_urgent", "admissions_programmed"]].sum().reset_index()
-    order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    df_weekly["day_of_week"] = pd.Categorical(df_weekly["day_of_week"], categories=order, ordered=True)
-    df_weekly = df_weekly.sort_values("day_of_week")
-    return(df_weekly)
+    if value_columns is None:
+        value_columns = ["admissions_urgent", "admissions_programmed"]
+
+    if aggregate_column not in df.columns:
+        raise ValueError(f"La colonne '{aggregate_column}' n'existe pas dans le dataset.")
+
+    df_grouped = df.groupby(aggregate_column)[value_columns].sum().reset_index()
+    if value_order is not None:
+        df_grouped[aggregate_column] = pd.Categorical(df_grouped[aggregate_column], categories=value_order,
+                                                      ordered=True)
+        df_grouped = df_grouped.sort_values(aggregate_column)
+
+    return (df_grouped)
+
 
 def create_admission_graph(df, value, title, label):
-        plt.figure(figsize=(10, 6))
-        bar_width = 0.4
-        x = range(len(df))
-        plt.bar(x, df["admissions_urgent"], width=bar_width, label="Admissions Urgentes", alpha=0.7, align='center')
-        plt.bar([i + bar_width for i in x], df["admissions_programmed"], width=bar_width, label="Admissions Programmées", alpha=0.7, align='center')
+    plt.figure(figsize=(10, 6))
+    bar_width = 0.4
+    x = range(len(df))
+    plt.bar(x, df["admissions_urgent"], width=bar_width, label="Admissions Urgentes", alpha=0.7, align='center')
+    plt.bar([i + bar_width for i in x], df["admissions_programmed"], width=bar_width, label="Admissions Programmées",
+            alpha=0.7, align='center')
 
-        plt.xlabel(label)
-        plt.ylabel("Nombre d'Admissions")
-        plt.title(title)
-        plt.xticks([i + bar_width / 2 for i in x], df["day_of_week"], rotation=45)
-        plt.legend()
+    plt.xlabel(label)
+    plt.ylabel("Nombre d'Admissions")
+    plt.title(title)
+    plt.xticks([i + bar_width / 2 for i in x], df[value], rotation=45)
+    plt.legend()
 
-        plt.show()
+    plt.show()
 
-
-create_admission_graph(get_admissions_data(), "day_of_week", "Admissions selon le jour de la semaine", "Jour de la semaine")
+# df = get_aggregated_data('season')
+# create_admission_graph(df, "season", "Admissions selon la saison",
+#                        "saison")
