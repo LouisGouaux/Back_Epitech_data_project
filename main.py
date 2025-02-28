a4df973671082a5b2dae161926be9403a439c119
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2025-02-28 22:37:47 +0100

    Refactor: Created a create_graph method for a reusable code

--- main.py
+++ main.py
@@ -16,26 +16,28 @@ def get_resources(date):
     return (df_filtered[["date"] + resource_column].to_dict(orient="records"))
 
 
-def graph_by_week_days():
+def get_admissions_data():
     df = import_dataset()
     df_weekly = df.groupby("day_of_week")[["admissions_urgent", "admissions_programmed"]].sum().reset_index()
     order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     df_weekly["day_of_week"] = pd.Categorical(df_weekly["day_of_week"], categories=order, ordered=True)
     df_weekly = df_weekly.sort_values("day_of_week")
+    return(df_weekly)
 
-    plt.figure(figsize=(10, 6))
-    bar_width = 0.4
-    x = range(len(df_weekly))
-    plt.bar(x, df_weekly["admissions_urgent"], width=bar_width, label="Admissions Urgentes", alpha=0.7, align='center')
-    plt.bar([i + bar_width for i in x], df_weekly["admissions_programmed"], width=bar_width, label="Admissions Programmées", alpha=0.7, align='center')
+def create_admission_graph(df, value, title, label):
+        plt.figure(figsize=(10, 6))
+        bar_width = 0.4
+        x = range(len(df))
+        plt.bar(x, df["admissions_urgent"], width=bar_width, label="Admissions Urgentes", alpha=0.7, align='center')
+        plt.bar([i + bar_width for i in x], df["admissions_programmed"], width=bar_width, label="Admissions Programmées", alpha=0.7, align='center')
 
-    plt.xlabel("Jour de la Semaine")
-    plt.ylabel("Nombre d'Admissions")
-    plt.title("Comparaison des Admissions selon les Jours de la Semaine")
-    plt.xticks([i + bar_width / 2 for i in x], df_weekly["day_of_week"], rotation=45)
-    plt.legend()
+        plt.xlabel(label)
+        plt.ylabel("Nombre d'Admissions")
+        plt.title(title)
+        plt.xticks([i + bar_width / 2 for i in x], df["day_of_week"], rotation=45)
+        plt.legend()
 
-    plt.show()
+        plt.show()
 
 
-graph_by_week_days()
+create_admission_graph(get_admissions_data(), "day_of_week", "Admissions selon le jour de la semaine", "Jour de la semaine")
