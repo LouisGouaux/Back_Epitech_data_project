afd7758126d1c7e625e8e8ff0fae00495b10e5fe
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2025-02-28 12:17:12 +0100

    Feat: Created get_resources methods
    Mthods returns all hospital resources by a selected date

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("./dataset/hospital_data.csv")
-print(df.head((5)))
+def import_dataset():
+    df = pd.read_csv("./dataset/hospital_data.csv")
+    return(df)
+
+def get_resources(date):
+    df = import_dataset()
+    df_filtered = df[df["date"] == date]
+    resource_column = [
+        "service", "beds_occupied", "beds_available", "staff_present"
+    ]
+    return(df_filtered[["date"] + resource_column].to_dict(orient="records"))
+
