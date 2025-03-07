e72f2e500a20bb676f35812b83fb93ffa42174ad
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2025-03-07 13:53:16 +0100

    Feat: Created get-daily-data route

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+import json
+from main import get_resources
 
 app = Flask(__name__)
 
@@ -8,6 +10,12 @@ def ping():
     return jsonify({
         'message': 'PONG'
     })
+
+@app.route('/api/get-daily_data')
+def get_daily_data():
+    df = get_resources('2024-02-01')
+    response = Response(json.dumps(df, ensure_ascii=False), content_type="application/json; charset=utf-8")
+    return response
 
 
 if __name__ == '__main__':
